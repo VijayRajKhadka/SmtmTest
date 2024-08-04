d45a6cb6f2f158c033da86fea7c2bd425ade82f8
vijay <vijayrajkhadka100@gmail.com>
2024-08-04 13:03:20 +0545

    Project Initiation

---
+++ app/home/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../style/homePage.css'
+const HomePage = () => {
+  return (
+    <div className='main-container'>
+        <div className='container-head'>
+                
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
