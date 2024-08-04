8a3704b444fb02d2d985faed58ddce186d3c0f5c
vijay <vijayrajkhadka100@gmail.com>
2024-08-04 16:26:10 +0545

    Review card completed. Test completed

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -7,14 +7,28 @@ const HomePage = () => {
     <div className='main-container'>
         <div className='container-head'>
                 <div className='nav-bar-container'></div>
-                <span>Chiyapasal Guff</span>
+                <span style={{fontSize:'15px'}}>Chiyapasal Guff</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                 </svg>
         </div>
         <ScrollNavbar/>
-        <ReviewCard/>
+        <ReviewCard 
+        userName="Tanake dai" 
+        shareName="SKBBL" 
+        time="33 m" 
+        buttonName= "Buy" 
+        review="Something is up with the stock. Aja dheraina badeko cha"
+        />
+
+        <ReviewCard 
+        userName="Dhanake dai" 
+        shareName="PLIE" 
+        time="33 m" 
+        buttonName= "Sell" 
+        review="My though on activity of sana kisan .. hope u like it "
+        />
     </div>
   )
 }
--- components/reviewCard.js
+++ components/reviewCard.js
@@ -1,16 +1,54 @@
-import React from 'react'
-import '../style/reviewCard.css'
+"use client"
+import React from 'react';
+import '../style/reviewCard.css';
 
-const reviewCard = () => {
+const reviewCard = (props) => {
+    function showMsg(msg){
+        console.log(msg)
+    }
   return (
+    <div>
     <div className='review-card-main-container'>
-        <div className='profile-image-container'></div>
-        <div className='profile-head-container'>
-            <span className='profile-name'>Tanake dai</span>
-            <button className='review-button'>SKBBL</button>
+      <div className='profile-image-container'></div>
+      <div className='profile-head-container'>
+        <div className='profile-info'>
+          <p>{props.userName}</p>
+          <span className='review-time'>{props.time}</span>
+          
         </div>
+        <button className='review-button' onClick={() => showMsg(props.review)}>{props.shareName}</button>
+      </div>
+
+    </div>
+    <div className='review-button-container'>
+            <span className='review-desc'>{props.review}</span>
+          <div className='action-buttons'>
+            <button className='buy-button' >{props.buttonName}</button>
+            <div className='button-container'>
+            <div className='action-button'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="gray" className="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+              </svg>
+              <span>10</span>
+            </div>
+            <div className='action-button'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="gray" className="bi bi-chat-square-text" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+              </svg>
+              <span>3</span>
+            </div>
+            <div className='action-button'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="gray" className="bi bi-share" viewBox="0 0 16 16">
+                <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3"/>
+              </svg>
+            </div>
+            </div>
+          </div>
+    </div>
+
     </div>
   )
 }
 
-export default reviewCard
+export default reviewCard;
